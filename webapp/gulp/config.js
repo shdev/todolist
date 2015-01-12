bf643f3739439451a7909f009b7b7606377aa9cd
Sebastian Holtz <mail@sh-dev.de>
2015-01-13 00:11:53 +0100

    babaysteps to gulp

---
+++ webapp/gulp/config.js
@@ -0,0 +1,78 @@
+
+var destBase = 'build';
+
+var dirs = {
+  dest : {
+    base : destBase,
+    css : destBase + '/css',
+    images : destBase + '/images',
+    fonts : destBase + '/fonts',
+    js : destBase + '/js',
+    js_ext : destBase + '/js/external' 
+  },
+  src : {
+    sass : 'scss',
+    files : 'files',
+    coffee : 'coffee',
+    compass : 'config/compass.rb'
+  }
+}
+
+
+module.exports = {
+  autoprefixer: {
+    browsers: [
+      'last 2 versions',
+      'safari 5',
+      'ie 8',
+      'ie 9',
+      'opera 12.1',
+      'ios 6',
+      'android 4'
+    ],
+    cascade: true
+  },
+  browsersync: {
+    development: {
+      server: {
+        baseDir: [dirs.dest.base]
+      },
+      port: 9999,
+      files: [
+        dirs.dest.css + '/*.css',
+        dirs.dest.js + '/**/*.js',
+        dirs.dest.images + '/**',
+        dirs.dest.fonts + '/*'
+      ]
+    }
+  },
+  compass: {
+    config_file: dirs.src.compass,
+    css: dirs.dest.css,
+    image: dirs.dest.images,
+    js: dirs.dest.js,
+    sass: dirs.src.sass,
+    sourcemap: false,
+    style: "compressed",
+    time: true,
+  },
+  delete: {
+    src: [dirs.dest.base]
+  },
+  dirs : dirs,
+  copyFiles: {
+    src:  dirs.src.files + '/**/*',
+    dest: dirs.dest.base
+  },
+  sass: {
+    src:  dirs.src.sass + '/**/*.{sass,scss}',
+    dest: dirs.dest.css,
+    options: {
+      noCache: true,
+      compass: true,
+      bundleExec: true,
+      sourcemap: false,
+      sourcemapPath: '../../_assets/scss'
+    }
+  },
+};
---
+++ webapp/gulp/tasks/development/sass.js
@@ -0,0 +1,46 @@
+var gulp         = require('gulp');
+var plumber      = require('gulp-plumber');
+var browsersync  = require('browser-sync');
+var gulpFilter   = require('gulp-filter');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps   = require('gulp-sourcemaps');
+var config       = require('../../config');
+var compass      = require('gulp-compass');
+
+/**
+ * Generate CSS from SCSS
+ * Build sourcemaps
+ */
+
+// gulp.task('sass', function () {
+//   return gulp.src(config.dirs.src.sass + '/**/*.scss')
+//     .pipe(plumber({
+//       errorHandler: function (error) {
+//         console.log(error.message);
+//         this.emit('end');
+//     }}))
+//     .pipe(compass(config.compass))
+//     .pipe(gulp.dest(config.dirs.dest.css));
+// });
+
+
+gulp.task('sass', function() {
+  var sassConfig = config.sass.options;
+
+  sassConfig.onError = browsersync.notify;
+
+  // Don’t write sourcemaps of sourcemaps
+  var filter = gulpFilter(['*.css', '!*.map']);
+
+  browsersync.notify('Compiling Sass');
+
+  return gulp.src(config.sass.src)
+    .pipe(plumber())
+    .pipe(compass(config.compass))
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer(config.autoprefixer))
+    .pipe(filter) // Don’t write sourcemaps of sourcemaps
+    .pipe(sourcemaps.write('.', { includeContent: false }))
+    .pipe(filter.restore()) // Restore original files
+    .pipe(gulp.dest(config.sass.dest));
+});

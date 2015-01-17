6949a37aa90bfa42e3aa1d2a0eb02509e5e439dc
Sebastian Holtz <mail@sh-dev.de>
2015-01-17 17:29:01 +0100

    remove gulp tasks

--- webapp/gulp/tasks/development/coffeelint.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var coffeelint = require('gulp-coffeelint');
-var config  = require('../../../gulpconfig').coffeelint;
-
-gulp.task('coffee:lint', function () {
-    gulp.src(config.src)
-        .pipe(coffeelint())
-        .pipe(coffeelint.reporter())
-});
--- webapp/gulp/tasks/production/optimize.js
+++
@@ -1,5 +0,0 @@
-var gulp     = require('gulp');
-
-gulp.task('optimize', ['optimize:html', 'optimize:js', 'optimize:images'] , function() {
-  
-});
